5ebb1362ef4432d73f4ccd46132dce674d848036
Layanne Alves <94229957+eiLayanneAlves@users.noreply.github.com>
2026-02-23 21:56:11 -0300

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE spotify_clone;
+
+USE spotify_clone;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(255)
+);
